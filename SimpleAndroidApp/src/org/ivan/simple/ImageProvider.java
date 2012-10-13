ac9f7856603cdff5d247a24fabfd2c5291d528ad
TanyaGaleyev <tanuwka.spb@gmail.com>
2012-10-13 23:20:27 +0400

    Update image provider to load each bitmap once (to prevent memory leaks)

--- SimpleAndroidApp/src/org/ivan/simple/ImageProvider.java
+++ SimpleAndroidApp/src/org/ivan/simple/ImageProvider.java
@@ -1,17 +1,23 @@
 package org.ivan.simple;
 
+import java.util.HashMap;
+
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 public class ImageProvider {
 	public static  Resources resources;
+	private static HashMap<Integer, Bitmap> images = new HashMap<Integer, Bitmap>();
 
 	private ImageProvider() {
 	}
 	
 	public static Bitmap getBitmap(int id) {
-		return BitmapFactory.decodeResource(resources, id);
+		if(!images.containsKey(id)) {
+			images.put(id, BitmapFactory.decodeResource(resources, id));
+		}
+		return images.get(id);
 	}
 
 }
