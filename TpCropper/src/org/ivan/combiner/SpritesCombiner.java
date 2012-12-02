a5a259e6e96b60d802648aeacbe5cd409a679b11
TanyaGaleyev <tanuwka.spb@gmail.com>
2012-12-02 15:06:58 +0400

    Tp cropper, sprite combiner

---
+++ TpCropper/src/org/ivan/combiner/SpritesCombiner.java
@@ -0,0 +1,112 @@
+package org.ivan.combiner;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpritesCombiner {
+	private static final String IMAGES_PATH = "./images/combiner/";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpritesCombiner combiner = new SpritesCombiner();
+		combiner.combineSprite("sprite1");
+	}
+	
+	public void combineSprite(String spriteDirPath) {
+		File spriteDir = new File(IMAGES_PATH + spriteDirPath);
+		File[] spriteDirs = spriteDir.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File file) {
+				if(file.isDirectory()) return true;
+				return false;
+			}
+		});
+		BufferedImage[] sequenceV = new BufferedImage[spriteDirs.length];
+		for(int j = 0; j < spriteDirs.length; j++) {
+			File[] images = spriteDirs[j].listFiles(new FileFilter() {
+				
+				@Override
+				public boolean accept(File file) {
+					String fileName = file.getName();
+					if(fileName.substring(fileName.length() - 4).equalsIgnoreCase(".png")) return true;
+					return false;
+				}
+			});
+			BufferedImage[] sequence = new BufferedImage[images.length];
+			try {
+				for(int i = 0; i < images.length; i++) {
+					File image = images[i];
+					sequence[i] = ImageIO.read(image);
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				return;
+			}
+			BufferedImage result = combineSprite(sequence);
+			if(result == null) return;
+			sequenceV[j] = result;
+		}
+		BufferedImage allResult = combineSpriteV(sequenceV);
+		if(allResult == null) return;
+		try {
+			ImageIO.write(allResult, "PNG", new File(IMAGES_PATH + spriteDir.getName() + ".png"));
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			return;
+		}
+	}
+	
+	public BufferedImage combineSprite(BufferedImage... sequence) {
+		if(sequence == null) return null;
+		if(sequence.length == 0) return null;
+		int singleWidth = sequence[0].getWidth();
+		int singleHeight = sequence[0].getHeight(); 
+		BufferedImage resultImage = new BufferedImage(
+				singleWidth * sequence.length,
+				singleHeight,
+				BufferedImage.TYPE_INT_ARGB);
+		for(int i = 0; i < sequence.length; i++) {
+			BufferedImage image = sequence[i];
+			if(image.getWidth() != singleWidth) return null;
+			if(image.getHeight() != singleHeight) return null;
+		}
+		for(int i = 0; i < sequence.length; i++) {
+			BufferedImage image = sequence[i];
+			int[] rgbArray = image.getRGB(0, 0, singleWidth, singleHeight, null, 0, singleWidth);
+			resultImage.setRGB(singleWidth * i, 0, singleWidth, singleHeight, rgbArray, 0, singleWidth);
+		}
+		return resultImage;
+	}
+	
+	public BufferedImage combineSpriteV(BufferedImage... sequence) {
+		if(sequence == null) return null;
+		if(sequence.length == 0) return null;
+		int singleWidth = sequence[0].getWidth();
+		int singleHeight = sequence[0].getHeight(); 
+		BufferedImage resultImage = new BufferedImage(
+				singleWidth,
+				singleHeight * sequence.length,
+				BufferedImage.TYPE_INT_ARGB);
+		for(int i = 0; i < sequence.length; i++) {
+			BufferedImage image = sequence[i];
+			if(image.getWidth() != singleWidth) return null;
+			if(image.getHeight() != singleHeight) return null;
+		}
+		for(int i = 0; i < sequence.length; i++) {
+			BufferedImage image = sequence[i];
+			int[] rgbArray = image.getRGB(0, 0, singleWidth, singleHeight, null, 0, singleWidth);
+			resultImage.setRGB(0, singleHeight * i, singleWidth, singleHeight, rgbArray, 0, singleWidth);
+		}
+		return resultImage;
+	}
+
+}
---
+++ TpCropper/src/org/ivan/cropper/Cropper.java
@@ -0,0 +1,60 @@
+package org.ivan.cropper;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Cropper {
+	public static void main(String[] args) {
+		Cropper cropper = new Cropper();
+		try {
+			cropper.cropImage("images/tocrop/cropper");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cropImage(String path) throws IOException {
+		int oneWidth = 70;
+		int oneHeight = 70;
+		int shiftWidth = 8;
+		BufferedImage source = ImageIO.read(new File(path + ".png"));
+		BufferedImage target = new BufferedImage(source.getWidth(null), source.getHeight(null) * 2, BufferedImage.TYPE_INT_ARGB);
+		int cols = 8;
+		int rows = 6;
+		for(int j = 0; j < rows; j++) {
+			for(int i = 0; i < cols; i++) {
+				if(j % 2 == 0) {
+					int shift = shiftWidth * (i + 1);
+					int partWidth = oneWidth - shift;
+					if(partWidth >= 0) { 
+						int[] shiftedpart = source.getRGB(oneWidth * i, j * oneHeight, partWidth, oneHeight, null, 0, partWidth);
+						target.setRGB(oneWidth * i + shift, 2 * j * oneHeight, partWidth, oneHeight, shiftedpart, 0, partWidth);
+					}
+					int backPartWidth = shift;
+					if(backPartWidth < oneWidth) {
+						int[] shiftedback = source.getRGB(oneWidth * (i + 1) - backPartWidth, j * oneHeight, backPartWidth, oneHeight, null, 0, backPartWidth);
+						target.setRGB(oneWidth * i, (2 * j + 1) * oneHeight, backPartWidth, oneHeight, shiftedback, 0, backPartWidth);
+					}
+				} else {
+					int shift = shiftWidth * (i + 1);
+					int partWidth = oneWidth - shift;
+					if(partWidth >= 0) { 
+						int[] shiftedpart = source.getRGB(oneWidth * i + shift, j * oneHeight, partWidth, oneHeight, null, 0, partWidth);
+						target.setRGB(oneWidth * i, 2 * j * oneHeight, partWidth, oneHeight, shiftedpart, 0, partWidth);
+					}
+					int backPartWidth = shift;
+					if(backPartWidth < oneWidth) {
+						int[] shiftedback = source.getRGB(oneWidth * i, j * oneHeight, backPartWidth, oneHeight, null, 0, backPartWidth);
+						target.setRGB(oneWidth * (i + 1) - backPartWidth, (2 * j + 1) * oneHeight, backPartWidth, oneHeight, shiftedback, 0, backPartWidth);
+					}
+				}
+			}
+		}
+		ImageIO.write(target, "PNG", new File(path + "_cropped.png"));
+	}
+
+}
