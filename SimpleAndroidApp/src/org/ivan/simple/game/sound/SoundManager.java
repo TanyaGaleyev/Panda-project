d6a3cc8cdb69078e33b25236f4c040c20941423c
Ivan Pavlukhin <pav401@mail.ru>
2014-06-05 13:53:05 +0400

    Sound res map. Add some new sounds

--- SimpleAndroidApp/src/org/ivan/simple/game/sound/SoundManager.java
+++ SimpleAndroidApp/src/org/ivan/simple/game/sound/SoundManager.java
@@ -3,6 +3,7 @@ package org.ivan.simple.game.sound;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.util.SparseArray;
 
 import org.ivan.simple.R;
 import org.ivan.simple.game.level.LevelCell;
@@ -12,67 +13,83 @@ import org.ivan.simple.game.motion.MotionType;
 
 public class SoundManager {
     private SoundPool soundPool;
-    private int jumpSoundId;
-    private int flySoundId;
-    private int dieSoundId;
-    private int winSoundId;
-    private int warSoundId;
-    private int coinSoundId;
-    private int throwSoundId;
-    private int angleSoundId;
+//    private int jumpSoundId;
+//    private int flySoundId;
+//    private int dieSoundId;
+//    private int winSoundId;
+//    private int warSoundId;
+//    private int coinSoundId;
+//    private int throwSoundId;
+//    private int angleSoundId;
     private AudioManager audioManager;
-	
-	public SoundManager(Context context) {
-		soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+    private SparseArray<Integer> soundMap = new SparseArray<Integer>();
+    private final Context context;
+
+    public SoundManager(Context context) {
+        this.context = context;
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        jumpSoundId = soundPool.load(context, R.raw.jump, 1);
-        flySoundId = soundPool.load(context, R.raw.fly, 1);
-        warSoundId = soundPool.load(context, R.raw.war, 1);
-        dieSoundId = soundPool.load(context, R.raw.detonate, 1);
-        winSoundId = soundPool.load(context, R.raw.win, 1);
-        coinSoundId = soundPool.load(context, R.raw.coin, 1);
-        throwSoundId = soundPool.load(context, R.raw.throw0, 1);
-        angleSoundId = soundPool.load(context, R.raw.angle, 1);
-	}
+        mapSound(R.raw.jump);
+        mapSound(R.raw.fly);
+        mapSound(R.raw.war);
+        mapSound(R.raw.detonate);
+        mapSound(R.raw.win);
+        mapSound(R.raw.coin);
+        mapSound(R.raw.throw0);
+        mapSound(R.raw.angle);
+        mapSound(R.raw.reduce);
+        mapSound(R.raw.slick);
+        mapSound(R.raw.tp);
+    }
+
+    private void mapSound(int resId) {
+        soundMap.put(resId, soundPool.load(context, resId, 1));
+    }
 
     public void playSound(Motion motion, Motion prevMotion, LevelCell nextCell, LevelCell prevCell) {
         MotionType mt = motion.getType();
         MotionType prevMt = prevMotion.getType();
+        if(prevCell.getFloor().getType() == PlatformType.REDUCE) {
+            playSound(R.raw.reduce);
+            return;
+        }
         switch(mt) {
             case JUMP:
                 if(motion.getStage() == 0) {
-                    playSound(jumpSoundId);
+                    playSound(R.raw.jump);
                 } else if (prevCell.getFloor().getType() == PlatformType.TRAMPOLINE) {
-                    playSound(warSoundId);
+                    playSound(R.raw.war);
                 }
                 break;
             case JUMP_LEFT:
             case JUMP_RIGHT:
                 if(prevMt == MotionType.JUMP || prevMt == MotionType.TP) {
-                } else if (prevCell.getFloor().getType() == PlatformType.ANGLE_LEFT ||
+                } else if(prevCell.getFloor().getType() == PlatformType.ANGLE_LEFT ||
                 prevCell.getFloor().getType() == PlatformType.ANGLE_RIGHT) {
-                    playSound(angleSoundId);
+                    playSound(R.raw.angle);
+                } else if(prevCell.getFloor().getType() == PlatformType.SLICK) {
+                    playSound(R.raw.slick);
                 } else {
-                    playSound(jumpSoundId);
+                    playSound(R.raw.jump);
                 }
                 break;
             case JUMP_LEFT_WALL:
             case JUMP_RIGHT_WALL:
-                playSound(coinSoundId);
+                playSound(R.raw.coin);
                 break;
             case FLY_LEFT:
             case FLY_RIGHT:
                 if(motion.getStage() == 0) {
-                    playSound(flySoundId);
+                    playSound(R.raw.fly);
                 }
                 break;
             case BEAT_ROOF:
-                playSound(warSoundId);
+                playSound(R.raw.war);
                 break;
             case THROW_LEFT:
             case THROW_RIGHT:
                 if(motion.getStage() == 0) {
-                    playSound(throwSoundId);
+                    playSound(R.raw.throw0);
                 }
                 break;
             case FALL:
@@ -81,26 +98,35 @@ public class SoundManager {
             case STICK_RIGHT:
             case MAGNET:
             case TP:
+                break;
             case TP_LEFT:
             case TP_RIGHT:
+                playSound(R.raw.tp);
+                break;
+            case STAY:
+                if(prevCell.getFloor().getType() == PlatformType.BRICK ||
+                        prevCell.getFloor().getType() == PlatformType.GLUE) {
+                } else {
+                    playSound(R.raw.jump);
+                }
                 break;
             default:
-                playSound(jumpSoundId);
+                playSound(R.raw.jump);
                 break;
         }
     }
 
     public void playDetonate() {
-        playSound(dieSoundId);
+        playSound(R.raw.detonate);
     }
 
     public void playWin() {
-        playSound(winSoundId);
+        playSound(R.raw.win);
     }
 
-    private void playSound(int soundId) {
+    private void playSound(int resId) {
         float volume = getVolume();
-        soundPool.play(soundId, volume, volume, 1, 0, 1f);
+        soundPool.play(soundMap.get(resId), volume, volume, 1, 0, 1f);
     }
 
     private float getVolume() {
