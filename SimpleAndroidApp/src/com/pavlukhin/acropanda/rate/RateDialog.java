7ce582e8f5f7a60f16029b86cd045a04dfd3cad1
Ivan Pavlukhin <pav401@mail.ru>
2014-07-24 19:33:30 +0400

    some rework and prepare to load assets

--- SimpleAndroidApp/src/com/pavlukhin/acropanda/rate/RateDialog.java
+++ SimpleAndroidApp/src/com/pavlukhin/acropanda/rate/RateDialog.java
@@ -2,6 +2,7 @@ package com.pavlukhin.acropanda.rate;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -75,25 +76,34 @@ public class RateDialog extends Dialog {
     }
 
     private void prepareButton(Button btn, int resId) {
+        prepareButton(btn, getContext().getResources().getDrawable(resId));
+    }
+
+    private void prepareButton(Button btn, String imagePath) {
+        Drawable icon = new BitmapDrawable(
+                pContext.app().getImageProvider().getBitmapAutoResizeNoCache(imagePath));
+        prepareButton(btn, icon);
+    }
+
+    private void prepareButton(Button btn, Drawable icon) {
         float btnHeight = btnHeight();
         btn.setHeight((int) btnHeight);
         btn.setTextSize(TypedValue.COMPLEX_UNIT_PX, btnHeight * TEXT_SIZE_PERCENT);
-        setLeftIcon(btn, resId);
+        setLeftIcon(btn, icon);
     }
 
-    private void setLeftIcon(Button btn, int resId) {
-        Drawable d = getContext().getResources().getDrawable(resId);
+    private void setLeftIcon(Button btn, Drawable icon) {
         int h =  (int) (pContext.app().displayHeight * BTN_HEIGHT_PERCENT);
         float iconHeight = h * ICON_PERCENT;
         int margin = (int) ((h - iconHeight) / 2);
-        float scale = iconHeight / d.getIntrinsicHeight();
-        d.setBounds(
+        float scale = iconHeight / icon.getIntrinsicHeight();
+        icon.setBounds(
                 margin,
                 0,
-                margin + (int) (scale * d.getIntrinsicWidth()),
-                (int) (scale * d.getIntrinsicHeight()));
-        btn.setCompoundDrawables(
-                d, null, null, null);
+                margin + (int) (scale * icon.getIntrinsicWidth()),
+                (int) (scale * icon.getIntrinsicHeight()));
+        btn.setCompoundDrawables(icon, null, null, null);
+//        btn.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
     }
 
     private void initListeners() {
